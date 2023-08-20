13c9043e0d6f88db26eb97d74c81cca3467cda90
Rutuja-117 <139907839+Rutuja-117@users.noreply.github.com>
2023-08-20 17:56:52 +0530

    Add files via upload
    The second experiment involves use of different data types:
    Integer takes whole numbers --> 1,2,4, etc. --> 4 bytes
    Float takes fractional numbers upto 7 decimal places --> 1.903, 0.9243 etc. --> 4 bytes
    Double takes fractional numbers upto 15 decimal places --> 1.9037849, 0.92438935364 etc. --> 8 bytes
    Character takes single characters enclosed in single quotes --> 'a', 'H', '$' etc. --> 1 byte
    Boolean takes either true (1) or false (0) --> 1 byte
    -->sizeof operator is used to find the size of the memory allocated to that particular data type
    -->if else loop was learnt

---
+++ exp2c.cpp
@@ -0,0 +1,20 @@
+/*  
+    Rutuja Navale
+    PRN:22070123118
+    Experiment no:2(c)
+*/
+#include <iostream>
+using namespace std;
+
+int main() {
+   bool imagecolor = true;
+   if(imagecolor){ //Checks whether imagecolor is true or not, and if true it prints the below statement or else, prints the statement in the else part.
+    cout<<"Image is colorfull";
+   }else{
+    cout<<"Image is not colorfull";
+   }
+return 0;
+}
+/*  OUTPUT
+Image is colorfull
+*/
